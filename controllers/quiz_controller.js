52134cff934dc39b4553ad8a80bad0099b273642
jjalberca <jj.alberca@alumnos.upm.es>
2015-05-06 17:27:33 +0200

    Despliegue en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,20 +1,53 @@
-
 var models = require('../models/models.js');
 
-// GET /quizes/question
-exports.question = function(req, res) {
-  models.Quiz.findAll().then(function(quiz) {
-    res.render('quizes/question', { pregunta: quiz[0].pregunta});
+// GET /quizes
+exports.index = function(req, res) {
+  
+  if (typeof req.query.search == "string" && req.query.search.trim() != ""){
+    models.Quiz.findAll({where: ["pregunta like ?", '%' + req.query.search + '%'], order: 'pregunta ASC'}).then(function(quizes){
+      res.render('quizes/index', { quizes: quizes, query: req.query.search });
+    });
+  }
+  else {
+    models.Quiz.findAll().then(function(quizes) {
+    res.render('quizes/index', { quizes: quizes });
+    });
+  }
+  models.Quiz.findAll().then(function(quizes) {
+    res.render('quizes/index.ejs', { quizes: quizes});
   })
 };
 
-// GET /quizes/answer
+/*exports.search = function (req, res) {
+  if (req.query.serach){
+    modes.Quiz.findAll({where: ["pregunta like ?", '%' +req.query.search+ '%'], order: 'pregunta ASC'}).then(function(){
+      res.render('quizes/index', { quizes: quizes });
+    });
+  }
+  else {
+    models.Quiz.findAll().then(function(quizes) {
+    res.render('quizes/index', { quizes: quizes});
+    })
+  }
+};
+*/
+
+// GET /quizes/:id
+exports.show = function(req, res) {
+  models.Quiz.find(req.params.quizId).then(function(quiz) {
+    res.render('quizes/show', { quiz: quiz});
+  })
+};
+
+// GET /quizes/:id/answer
 exports.answer = function(req, res) {
-  models.Quiz.findAll().then(function(quiz) {
-    if (req.query.respuesta === quiz[0].respuesta) {
-      res.render('quizes/answer', { respuesta: 'Correcto' });
+  models.Quiz.find(req.params.quizId).then(function(quiz) {
+    if (req.query.respuesta === quiz.respuesta) {
+      res.render('quizes/answer', 
+                 { quiz: quiz, respuesta: 'Correcto' });
     } else {
-      res.render('quizes/answer', { respuesta: 'Incorrecto'});
+      res.render('quizes/answer', 
+                 { quiz: quiz, respuesta: 'Incorrecto'});
     }
   })
 };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-//var author = require('../routes/author.js')
 
 var quizController = require('../controllers/quiz_controller');
 
@@ -10,8 +9,10 @@ router.get('/', function(req, res) {
 });
 
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+//router.get('/quizes?search=', quizController.index);
 
 router.get('/author', function(req, res){
   res.render('author', {title: 'Créditos' });
