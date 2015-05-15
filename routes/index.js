846c2f521425d0ed27c2166f62cfca72ac8e7980
jjalberca <jj.alberca@alumnos.upm.es>
2015-05-15 19:08:56 +0200

    Editar preguntas

--- routes/index.js
+++ routes/index.js
@@ -13,14 +13,16 @@ router.param('quizId', quizController.load);  // autoload :quizId
 
 
 router.get('/quizes', quizController.index);
-router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new',                  quizController.new);
 router.post('/quizes/create',              quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
+router.put('/quizes/:quizId(\\d+)',        quizController.update);
 
 
 router.get('/author', function(req, res){
-  res.render('author', {title: 'Créditos' });
+  res.render('author', {title: 'Créditos', errors:[]});
 });
 
 module.exports = router;
